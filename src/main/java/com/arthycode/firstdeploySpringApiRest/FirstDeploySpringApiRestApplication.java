f677b6c1b89a7bbc795f4092795ad7fc486ab24f
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-05-17 11:25:28 -0600

    Commit para subir ejemplo de deploy.

---
+++ src/main/java/com/arthycode/firstdeploySpringApiRest/FirstDeploySpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.arthycode.firstdeploySpringApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDeploySpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDeploySpringApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthycode/firstdeploySpringApiRest/FirstDeploySpringApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthycode.firstdeploySpringApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstDeploySpringApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
